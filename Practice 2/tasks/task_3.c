bef475434dc3ab45a73eb11433669814587cc95d
Aiave <kiterin.kato@gmail.com>
2023-11-13 23:51:36 +0300

    Добавлено 3 задание.

--- Practice 2/tasks/task_3.c
+++ Practice 2/tasks/task_3.c
@@ -7,8 +7,34 @@ void task_3()
 
     printf("Условие задачи (100 баллов):\nОпределите число суток с А года по В год (учтите високосные годы).\n\n");
 
-    //ввод года 1 и года 2
-    //проверка на високосность
-    //массивы с количеством дней
-    //вызывать подсчёт разных массивов фором
+    int year_A, year_B;
+    printf("Введите год A: ");
+    scanf_s("%d", &year_A);
+    printf("Введите год B: ");
+    scanf_s("%d", &year_B);
+
+    int result = days_between_years(year_A, year_B);
+    printf("Число суток с %d года по %d год (включительно): %d\n", year_A, year_B, result);
+}
+
+int is_leap_year(int year)
+{
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+int days_in_year(int year)
+{
+    return is_leap_year(year) ? 366 : 365;
+}
+
+int days_between_years(int yearA, int yearB)
+{
+    int days = 0;
+
+    for (int year = yearA; year <= yearB; ++year)
+    {
+        days += days_in_year(year);
+    }
+
+    return days;
 }
---
+++ Practice 2/tasks/task_3.h
@@ -0,0 +1,8 @@
+﻿#ifndef PRACTISE_2_TASK_3
+#define PRACTISE_2_TASK_3
+
+int is_leap_year(int year);
+int days_in_year(int year);
+int days_between_years(int yearA, int yearB);
+
+#endif
